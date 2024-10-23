40988a722e4c398923700acef371382b1b93fdfd
ASUS <ASUS@LAPTOP-F8GF5CCM.dhcp.wustl.edu>
2024-10-23 14:17:00 -0500

    yer

--- src/studio6/RecursiveMethods.java
+++ src/studio6/RecursiveMethods.java
@@ -12,9 +12,12 @@ public class RecursiveMethods {
 	 *         ...)
 	 */
 	public static double geometricSum(int n) {
-		
-			// FIXME compute the geometric sum for the first n terms recursively
+		if (n == 0) {
 			return 0;
+		} else {
+			return Math.pow(0.5, n) + geometricSum(n-1);
+		}
+			
 		
 	}
 	
@@ -30,7 +33,15 @@ public class RecursiveMethods {
 	public static void circlesUponCircles(double xCenter, double yCenter, double radius,
 			double radiusMinimumDrawingThreshold) {
 		
-		// FIXME
+		if (radius < radiusMinimumDrawingThreshold) {
+			return;
+		} else {
+			StdDraw.circle(xCenter, yCenter, radius);
+			circlesUponCircles(xCenter+radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter-radius, yCenter, radius/3.0, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter, yCenter+radius, radius/3.0, radiusMinimumDrawingThreshold);
+			circlesUponCircles(xCenter, yCenter-radius, radius/3.0, radiusMinimumDrawingThreshold);
+		}
 	}
 
 	/**
